85039afd2ef4ba86287ffbd07e192fedb2621e07
Viktoriia P. <pohrebniakvika@gmail.com>
2026-01-14 08:11:43 +0100

    changed errorYandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,17 +1,23 @@
 import createHttpError from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-  if (req.log?.error) req.log.error(err);
-  else console.error(err);
+  const status = err.status || 500;
+
+  if (req.log) {
+    if (status >= 500) req.log.error(err);
+    else req.log.warn({ err }, err.message);
+  } else {
+    console.error(err);
+  }
 
   if (createHttpError.isHttpError(err)) {
-    return res.status(err.status).json({ message: err.message });
+    return res.status(status).json({ message: err.message || err.name });
   }
 
   const isProd = process.env.NODE_ENV === 'production';
   return res.status(500).json({
     message: isProd
       ? 'Something went wrong. Please try again later.'
-      : err?.message || 'Error',
+      : err.message,
   });
 };
